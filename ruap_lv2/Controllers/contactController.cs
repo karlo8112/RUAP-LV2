2057afc2b8399450efb6fa34b067a3a07571c446
karlo81121 <adzic.karlo74@gmail.com>
2021-11-18 19:13:14 +0100

    Task 1 and Task 2

---
+++ ruap_lv2/Controllers/contactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ruap_lv2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class contactController : ControllerBase
+    {
+        public string[] Get()
+        {
+            return new string[]
+            {
+        "Hello",
+        "World"
+            };
+        }
+    }
+}
